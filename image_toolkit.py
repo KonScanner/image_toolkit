6159e706859caf1c1d171e6a58dfa266f4460583
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-05-02 17:39:32 +0100

    updated_img_type

--- image_toolkit.py
+++ image_toolkit.py
@@ -4,32 +4,32 @@ import sys
 
 
 class Image_stuff:
-    def __init__(self, input_path, output_path):
+    def __init__(self, input_path, output_path, img_type='.png'):
         self.input_path = input_path
         self.output_path = output_path
+        self.img_type = img_type
         self.image_dirs = self._image_dirs()
         self.file_names = self._image_names()
-        self.image_type = '.png'
 
     def _image_dirs(self):
         return [
             str(self.input_path) + '/' + file for file in os.listdir(self.input_path)]
 
     def _image_names(self):
-        return [file.strip(self.image_type) for file in os.listdir(self.input_path)]
+        return [file.strip(self.img_type) for file in os.listdir(self.input_path)]
 
     def _greyscale(self):
         for c, image in enumerate(self.image_dirs):
             img = Image.open(image).convert('LA')
             img.save(str(self.output_path) + '/' +
-                     '{}_greyscale.{}'.format(self.file_names[c], self.image_type))
+                     '{0}_greyscale.{1}'.format(self.file_names[c], self.img_type))
 
     def _resize_symmetric(self, scale):
         for c, image in enumerate(self.image_dirs):
             img = Image.open(image)
             img = img.resize((scale, scale), Image.ANTIALIAS)
             img.save(str(self.output_path) + '/' +
-                     '{}_rescaled_sym.{}'.format(self.file_names[c], self.image_type))
+                     '{0}_rescaled_sym.{1}'.format(self.file_names[c], self.img_type))
 
     def _resize_proportionally(self, width):
         for c, image in enumerate(self.image_dirs):
@@ -38,13 +38,13 @@ class Image_stuff:
             hsize = int((float(img.size[1])*float(wpercent)))
             img = img.resize((width, hsize), Image.ANTIALIAS)
             img.save(str(self.output_path) + '/' +
-                     '{}_rescaled.{}}'.format(self.file_names[c], self.image_type))
+                     '{0}_rescaled.{1}'.format(self.file_names[c], self.img_type))
 
     def _mirror(self):
         for c, image in enumerate(self.image_dirs):
             img = Image.open(image).transpose(Image.FLIP_LEFT_RIGHT)
             img.save(str(self.output_path) + '/' +
-                     '{}_mirrored.{}'.format(self.file_names[c], self.image_type))
+                     '{0}_mirrored.{1}'.format(self.file_names[c], self.img_type))
 
 
 if __name__ == "__main__":
